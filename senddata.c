8689489bab3dabc7c1217810a472f665be0a6c2a
Azrifaiz <2021843172@student.uitm.edu.my>
2022-06-24 13:08:40 +0000

    adding python code

--- senddata.c
+++ senddata.c
@@ -6,6 +6,7 @@ int main(int argc , char *argv[])
 {
 	int socket_desc;
 	struct sockaddr_in server, client;
+              char *message;
 	//Create socket
 	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 	if (socket_desc == -1)
@@ -14,7 +15,7 @@ int main(int argc , char *argv[])
 	}
 	server.sin_addr.s_addr = inet_addr("192.168.248.2"); //Please enter the ip address of your Server VM
 	server.sin_family = AF_INET;
-	server.sin_port = htons( 8888 );
+	server.sin_port = htons( 22 );
 
 	//Connect to remote server
 	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
@@ -22,7 +23,17 @@ int main(int argc , char *argv[])
 		puts("connect error");
 		return 1;
 	}
-	puts("Connected");
+	puts("Connected \n");
+
+	//Send some data
+	size_t strlen(const char *str);
+	message = "connect";
+	if( send(socket_desc , message , strlen(message) , 0) < 0)
+	{
+		puts("Send failed");
+		return 1;
+	}
+	puts("Data Send\n");
 	return 0;
 }
 
